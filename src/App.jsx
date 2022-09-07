e9bfe04c5c3e7a166c888d42109efdbc8518283c
Ivan <Priest.ivan14@gmail.com>
2022-09-07 20:36:49 +0300

    Создание формы подиски: Email+Checkbox+Button Send + Validation Email+Checkbox Function in Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,69 @@
 
 import './App.css';
 import React, { Component } from "react";
-import {Posts} from "./component/posts";
 
 
   class App extends Component {
  
     state ={
-      posts: [
-        {id:"abc1", name:"JS Basics"},
-        {id:"abc2", name:"Js Advanced"},
-        {id:"abc3", name:"ReactJS"},
-      ],
+      email: "",
+      regulations: false
     };
 
-    removePost = (id)=> {      
-      this.setState({posts: this.state.posts.filter(post=> post.id !== id)})
+
+    handleSubmit = ()=> {
+      const isValidEmail = /^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i.test(this.state.email);
+      const isValidCheckbox = this.state.regulations;
+      if(!isValidEmail){
+        alert("Вы ввели некорректный Email")
+        return
+      }
+      if(!isValidCheckbox){
+        alert("Вы не согласились с правилами и условиями")
+        return
+      }
+      this.setState({email:"", regulations: false})
+      alert("Спасибо! Вы подписались!")
+    }
+
+    handleChange =(event) => {
+      this.setState({[event.target.name] : event.target.value})
+    }
+
+    handleCheckboxChange =(event) => {
+      this.setState({[event.target.name]: event.target.checked})
     }
 
     
 
   render() {
 
-    const{posts} = this.state;
+    const{email, regulations} = this.state;
 
     return (
-      <div className='App'>
+      <div>
+          <input
+          type="email"
+          name="email"
+          placeholder='email'
+          value={email}        
+          onChange={this.handleChange}
+          onBlur={this.validateEmail}            
+          />
 
-      <Posts posts ={posts} removePost={this.removePost}/>
+          <br />
+
+          <label>
+          <input
+          type="checkbox"
+          name="regulations"
+          checked={regulations}
+          onChange={this.handleCheckboxChange}
+          /> Я согласен с правилами и условиями
+          </label>
+         <br />
+
+         <button onClick={()=> {this.handleSubmit()}}>Отправить</button>
 
       </div>
     )
--- src/component/post.jsx
+++
@@ -1,7 +0,0 @@
-export function Post(props) {
-
-    // Диструктиризация props, чтоб каждый раз не писать props.cb и props.name
-    const{id, name, removePost} = props;
-
-    return <h2>{name}<button onClick={() => removePost(id)} style={{margin: "10px"}}>Удалить</button></h2>
-}
