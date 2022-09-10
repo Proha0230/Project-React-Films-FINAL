adac2741f64e67d144d67c7f9585eb5f5bb7ee39
Ivan <Priest.ivan14@gmail.com>
2022-09-10 13:57:35 +0300

    Версия 3.0 Добавлена строка поиска и ее программная установка по поиску в IPI OMBdb API Добавлена фильтрация выдаваемого содержимого по фильтра All/Movies/Series Стилизован Input Search и т.д

--- src/component/Movie.jsx
+++ src/component/Movie.jsx
@@ -5,13 +5,18 @@ function Movie (props) {
           Type, 
           Poster} = props;
     
-    return <div id={ImdbID} class="card movie">
-    <div class="card-image waves-effect waves-block waves-light">
-      <img class="activator" src={Poster}/>
+    return <div id={ImdbID} className="card movie">
+    <div className="card-image waves-effect waves-block waves-light">
+      {
+        Poster === "N/A" ?
+      <img className="activator" src={`https://via.placeholder.com/300x420?text=${Title}`} alt={Title}/>
+        :
+      <img className="activator" src={Poster} alt={Title}/>
+      }
     </div>
-    <div class="card-content">
-      <span class="card-title activator grey-text text-darken-4">{Title}
-      <i class="material-icons right">{Year}</i>
+    <div className="card-content">
+      <span className="card-title activator grey-text text-darken-4">{Title}
+      <i className="material-icons right">{Year}</i>
       </span>
       <p>{Type}</p>
     </div>
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { Movies } from "../component/Movies";
+import {Movies} from "../component/Movies";
+import {Preloader} from "../component/preloader";
+import {Search} from "../component/Search";
 
 class Main extends React.Component {
     state ={
@@ -7,18 +9,26 @@ class Main extends React.Component {
     }
 
 
-componentDidMount() {
-    fetch("http://www.omdbapi.com/?apikey=78584b3c&s=Matrix")
+componentDidMount(str, type = "all") {
+    fetch(`http://www.omdbapi.com/?apikey=78584b3c&s=${str}${type !=="all" ? `&type=${type}` : ""}`)
     .then (response => response.json())
     .then (data => this.setState({movies: data.Search}))
 }
 
+searchMovies = (str, type = "all") => {
+    fetch(`http://www.omdbapi.com/?apikey=78584b3c&s=${str}${type !=="all" ? `&type=${type}` : ""}`)
+    .then (response => response.json())
+    .then (data => this.setState({movies: data.Search}))
+}
+
+
 render() {
     const{movies} = this.state;
     return <main className="container content">
+        <Search searchMovies={this.searchMovies}/>
         {movies.length ? (
             <Movies movies={this.state.movies}/>
-        ) : <h3>Loading...</h3>
+        ) : <Preloader />
         }
     </main>
 }
