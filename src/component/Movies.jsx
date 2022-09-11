13a291bb037d8600543d2bdf48367b78b6d9aa88
Ivan <Priest.ivan14@gmail.com>
2022-09-11 10:24:03 +0300

    Финальная версия проекта сайта с Фильмами. Что сделано: добавлено выведение сообщения в случае некорректного запроса в поисковой строке - Ничего не найдено, теперь сайт не крашится и не показывает белый экран а выводит это сообщение. Обезопасил свой API KEY создав файл в public/.env.local c указанием ключа и внедрением его в Main.jsx и использование в fetch запросах

--- src/component/Movies.jsx
+++ src/component/Movies.jsx
@@ -1,12 +1,12 @@
 import { Movie } from "./Movie";
 
 function Movies (props) {
-    const{movies} = props;
+    const{movies=[]} = props;
 
     return <div className="movies">
-        {movies.map (movie => (
+        {movies.length ? movies.map (movie => (
             < Movie key = {movie.ImdbID}{...movie}/>
-        ))}
+        )) : <h4 className="TextnotFound">Ничего не найдено :( </h4>}
     </div>
 }
 
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -3,33 +3,34 @@ import {Movies} from "../component/Movies";
 import {Preloader} from "../component/preloader";
 import {Search} from "../component/Search";
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 class Main extends React.Component {
     state ={
         movies:[],
+        loading: true,
     }
 
 
 componentDidMount(str, type = "all") {
-    fetch(`http://www.omdbapi.com/?apikey=78584b3c&s=${str}${type !=="all" ? `&type=${type}` : ""}`)
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !=="all" ? `&type=${type}` : ""}`)
     .then (response => response.json())
-    .then (data => this.setState({movies: data.Search}))
+    .then (data => this.setState({movies: data.Search, loading: false}))
 }
 
 searchMovies = (str, type = "all") => {
-    fetch(`http://www.omdbapi.com/?apikey=78584b3c&s=${str}${type !=="all" ? `&type=${type}` : ""}`)
+    this.setState({loading: true})
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !=="all" ? `&type=${type}` : ""}`)
     .then (response => response.json())
-    .then (data => this.setState({movies: data.Search}))
+    .then (data => this.setState({movies: data.Search, loading: false}))
 }
 
 
 render() {
-    const{movies} = this.state;
+    const{movies, loading} = this.state;
     return <main className="container content">
         <Search searchMovies={this.searchMovies}/>
-        {movies.length ? (
-            <Movies movies={this.state.movies}/>
-        ) : <Preloader />
-        }
+        {loading ? <Preloader /> : <Movies movies={movies}/>}
     </main>
 }
 }
